4952844cc2d9c9cd223f6fa7e1c8b9ddc8dceb8a
Knoppix User <knoppix@Microknoppix.(none)>
2014-03-29 23:09:25 +0000

    fix import

--- hellodjango/wsgi.py
+++ hellodjango/wsgi.py
@@ -14,6 +14,9 @@ application = get_wsgi_application()
 
 """
 
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellodjango.settings")
+
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
